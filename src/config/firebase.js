9d02567b8d10b35924ddb0a781cc4545e5c05735
PeterHW963 <peterhadiwijaya963@gmail.com>
2026-03-21 12:00:13 +0800

    change expenseService functions and function calls in App.jsx

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// ADD YOUR FIREBASE WEB APP CONFIG HERE
+// FIREBASE WEB APP CONFIG
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
