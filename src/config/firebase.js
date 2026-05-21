2519f482dce79caf0e294bbbe58f69f32fdfe4ab
PeterHW963 <peterhadiwijaya963@gmail.com>
2026-05-22 01:56:21 +0800

    step 5
    enable anonymous auth on firebase console then
    create the function for anonymous authentication to use in app

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, signInAnonymously } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +23,33 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const db = getFirestore(); // get DB instance and use it in app
+const auth = getAuth(); // get auth manager instance of the project to use in app
 
-export { app, db };
+export const anonymousAuth = async () => {
+  if (auth.currentUser) {
+    return auth.currentUser;
+  }
+
+  const credential = await signInAnonymously(auth);
+  return credential.user;
+};
+
+// need to import the fns
+// // Register new user
+// export const registerWithEmail = async (email, password) => {
+//   const credential = await createUserWithEmailAndPassword(auth, email, password);
+//   return credential.user;
+// };
+
+// // Sign in existing user
+// export const loginWithEmail = async (email, password) => {
+//   const credential = await signInWithEmailAndPassword(auth, email, password);
+//   return credential.user;
+// };
+
+// // Sign out
+// export const logout = async () => {
+//   await signOut(auth);
+// };
+
+export { app, auth, db };
