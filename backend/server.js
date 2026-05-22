a2724c4b2aab78aa8374ea7df939f174c3a85e2f
PeterHW963 <peterhadiwijaya963@gmail.com>
2026-05-23 00:56:26 +0800

    step 2
    setup db configturation
    call the connectDB function on server.js's startServer()

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -19,6 +20,15 @@ app.use(
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server: ", error);
+  }
+}
+
+startServer();
