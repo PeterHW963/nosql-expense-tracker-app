a16fe17049c9258b0c3a7445d89aeeaa094ee29d
PeterHW963 <peterhadiwijaya963@gmail.com>
2026-05-23 00:53:07 +0800

    step 1.6
    setup cors and express.json in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  }),
+);
+
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
