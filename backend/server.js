85036d35fa583cd1dc6a1d53a55d6031a614c827
PeterHW963 <peterhadiwijaya963@gmail.com>
2026-05-22 23:58:17 +0800

    step 1.5
    add npm script for npm run dev
    add Hello world to show on port 3000

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
